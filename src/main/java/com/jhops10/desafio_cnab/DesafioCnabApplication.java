306408338efe6d435eccbab83a6fd5758905122e
João Paulo <joaowais@gmail.com>
2025-03-19 23:25:00 -0300

    first commit

---
+++ src/main/java/com/jhops10/desafio_cnab/DesafioCnabApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.desafio_cnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCnabApplication.class, args);
+	}
+
+}
